e5a1c43a17fb4eaa5a5b02edb4ed1c77ac9cc246
Joshideepu <joshideepu2001@gmail.com>
2023-07-05 23:43:57 +0530

    Added required files

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Lovely",
+        artist: "Billie Eilish",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Perfect",
+        artist: "Ed Sheeran",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Play date",
+        artist: "Melanie Martinez",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Positions",
+        artist: "Ariana grande",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "See You Again",
+        artist: "Wiz Khalifa",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "music-6",
+        src: "music-6"
+    },
+]
